4d5104946f44b884ed42b7790c3e979e4eece59f
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-01-26 18:11:17 +0200

    update slider

---
+++ works/bouncy/js/app.js
@@ -0,0 +1,18 @@
+
+
+
+$(function($) {
+
+    /*Team Slider: https://github.com/kenwheeler/slick/*/
+    let slider = $("#teamSlider");
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false
+    });
+
+
+});
+
--- works/bouncy/js/portfolio.js
+++ works/bouncy/js/portfolio.js
@@ -1,5 +1,8 @@
 
 jQuery(function($) {
+
+
+
     $('.grid').masonry({
         itemSelector: '.grid-item',
         columnWidth: 280
