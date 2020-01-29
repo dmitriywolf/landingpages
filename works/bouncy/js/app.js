71b2a0f994adb42cb6786eb68d507a9935853682
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-01-29 23:52:34 +0200

    add favicon

--- works/bouncy/js/app.js
+++ works/bouncy/js/app.js
@@ -1,52 +1,12 @@
-// $(function ($) {
-//     //Portfolio: https://github.com/desandro/masonry
-//     $('.grid').masonry({
-//         itemSelector: '.grid-item',
-//         columnWidth: 280
-//     });
-//
-//
-//
-//
-//
-//
-//     //Team Slider: https://github.com/kenwheeler/slick/
-//     let slider = $("#teamSlider");
-//
-//     slider.slick({
-//         infinite: true,
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         fade: false,
-//         arrows: false,
-//         dots: true
-//     });
-//
-//     //Testimonials Slider
-//     let testSlider = $("#testimonialsSlider");
-//
-//     testSlider.slick({
-//         infinite: true,
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         fade: true,
-//         arrows: false,
-//         dots: true
-//     });
-//
-//
-// });
-
-
 $(function () {
 
-    //Фиксированная шапка
+    //Fixed Header
     let header = $("#header");
     let intro = $("#intro");
-    let introH = intro.innerHeight();
+    let introH;
     let scrollPos = $(window).scrollTop();
 
-    $(window).on("scroll load", function () {
+    $(window).on("scroll load resize", function () {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
 
@@ -57,6 +17,67 @@ $(function () {
         }
     });
 
+
+    //Smooth Scroll
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+        navToggle.removeClass("burger--close"); //Удаляем клас измениения бургера
+
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 50
+        }, 1000);
+    });
+
+
+    //Nav Toggle
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+        header.toggleClass("header-bg"); //Меняем цвет фона для Header
+        navToggle.toggleClass("burger--close"); //Добавляем клас изменения бургера
+    });
+
+
+    //Team Slider: https://github.com/kenwheeler/slick/
+    let slider = $("#teamSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        arrows: false,
+        dots: true
+    });
+
+    //Testimonials Slider
+    let testSlider = $("#testimonialsSlider");
+
+    testSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
+
+    //Portfolio: https://github.com/desandro/masonry
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 280
+    });
 
 });
 
