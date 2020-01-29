5f137ee1c224172893098f5a5b3ce097cc7b395b
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-01-29 21:55:26 +0200

    menu

--- works/bouncy/js/app.js
+++ works/bouncy/js/app.js
@@ -1,56 +1,60 @@
-$(function ($) {
+// $(function ($) {
+//     //Portfolio: https://github.com/desandro/masonry
+//     $('.grid').masonry({
+//         itemSelector: '.grid-item',
+//         columnWidth: 280
+//     });
+//
+//
+//
+//
+//
+//
+//     //Team Slider: https://github.com/kenwheeler/slick/
+//     let slider = $("#teamSlider");
+//
+//     slider.slick({
+//         infinite: true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         fade: false,
+//         arrows: false,
+//         dots: true
+//     });
+//
+//     //Testimonials Slider
+//     let testSlider = $("#testimonialsSlider");
+//
+//     testSlider.slick({
+//         infinite: true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         fade: true,
+//         arrows: false,
+//         dots: true
+//     });
+//
+//
+// });
 
 
+$(function () {
 
+    //Фиксированная шапка
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
 
+    $(window).on("scroll load", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //Portfolio: https://github.com/desandro/masonry
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 280
-    });
-
-
-
-
-
-
-    //Team Slider: https://github.com/kenwheeler/slick/
-    let slider = $("#teamSlider");
-
-    slider.slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: false,
-        arrows: false,
-        dots: true
-    });
-
-    //Testimonials Slider
-    let testSlider = $("#testimonialsSlider");
-
-    testSlider.slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        arrows: false,
-        dots: true
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
     });
 
 
