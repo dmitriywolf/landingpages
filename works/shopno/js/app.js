c4e53b8a281fe0b35b4cfab8be859fdd9786f568
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-02-10 20:50:03 +0200

    add js

---
+++ works/shopno/js/app.js
@@ -0,0 +1,52 @@
+$(function () {
+
+    //Fixed Header
+    let header = $("#header");
+    let intro = $("#intro");
+    let introHeight = intro.innerHeight();
+    let scrollPosition = $(window).scrollTop();
+
+    $(window).on("scroll load resize", function () {
+        introHeight = intro.innerHeight();
+
+        scrollPosition = $(this).scrollTop();
+
+        if (scrollPosition > introHeight) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    });
+
+
+    //Smooth Scroll
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+        navToggle.removeClass("burger--close");
+
+        $("html, body").animate({
+            scrollTop: elementOffset
+        }, 1200);
+    });
+
+    //Nav Toggle
+
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+        navToggle.toggleClass("burger--close");
+
+
+    });
+
+
+});
