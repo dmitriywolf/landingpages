01cba1a1d68996dfeb16556c9c058e2174affd64
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-03-10 20:23:18 +0200

    add clock

--- works/irvas/js/app.js
+++ works/irvas/js/app.js
@@ -1,3 +1,4 @@
+//Tab
 let tab = function () {
     let tabNav = document.querySelectorAll('.glazing-nav-tab');
     let tabContent = document.querySelectorAll('.glazing-tab-content');
@@ -24,3 +25,51 @@ let tab = function () {
 };
 
 tab();
+
+
+//Timer
+
+let dedline = '2020-04-23';
+
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t / 1000) % 60),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        days = timer.querySelector('.days'),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minutes.innerHTML = t.minutes;
+        seconds.innerHTML = t.seconds;
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+
+    updateClock();
+
+    let timeInterval = setInterval(updateClock, 1000);
+}
+
+setClock('timer', dedline);
+
