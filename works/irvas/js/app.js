b59f8d80825da5a2de31bc9f681f8f3a1bc14929
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-03-14 18:28:22 +0200

    update tab

--- works/irvas/js/app.js
+++ works/irvas/js/app.js
@@ -1,5 +1,6 @@
-//Tab
-let tab = function () {
+
+// Tab Glazing
+let tabGlazing = function () {
     let tabNav = document.querySelectorAll('.glazing-nav-tab');
     let tabContent = document.querySelectorAll('.glazing-tab-content');
     let tabName;
@@ -24,7 +25,49 @@ let tab = function () {
     }
 };
 
-tab();
+tabGlazing();
+
+//Tab Decoration
+let tabDecoration = function () {
+    let tabNavDecoration = document.querySelectorAll('.decoration-nav-tab');
+    let tabContentDecoration = document.querySelectorAll('.decoration-tab-content');
+    let tabNameDecoration;
+
+    tabNavDecoration.forEach(item => {
+        item.addEventListener('click', selectTabNavDecoration)
+    });
+
+    function selectTabNavDecoration() {
+        tabNavDecoration.forEach(item => {
+            item.classList.remove('is-active');
+        });
+        this.classList.add('is-active');
+        tabNameDecoration = this.getAttribute('data-tab-name');
+        selectTabContent(tabNameDecoration);
+    }
+
+    function selectTabContent(tabNameDecoration) {
+        tabContentDecoration.forEach(item => {
+            item.classList.contains(tabNameDecoration) ? item.classList.add('is-active') : item.classList.remove('is-active');
+        })
+    }
+};
+
+tabDecoration();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Timer
