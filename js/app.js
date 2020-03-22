349578fd67956df258223165d97c0743e0b0fa29
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-03-22 15:07:51 +0200

    update navigation

--- js/app.js
+++ js/app.js
@@ -6,18 +6,51 @@ $(function () {
     let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
 
-    $(window).on("scroll", function () {
+    checkScroll(scrollPos, introH);
 
+    $(window).on("scroll resize", function () {
+        introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
 
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
         if (scrollPos > introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
+    }
+
+    /* Navigation Smooth Scroll
+    =============================*/
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass('show');
+        navToggle.removeClass('burger--close');
+        header.removeClass('show');
+
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 65
+        }, 1200);
     });
 
+    /* Navigation Burger Nav Toggle
+    =============================*/
+    let nav = $("#navigation");
+    let navToggle = $("#navToggle");
 
-    // header.addClass("fixed");
+    navToggle.on("click", function (event) {
+        event.preventDefault();
 
+        header.toggleClass('show');
+        nav.toggleClass('show');
+        navToggle.toggleClass('burger--close');
+
+    })
 });
