dac4ba0efeefedbcea28204f4780562d0aefc3d4
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-04-18 19:16:29 +0300

    add smooth scroll

--- works/23Degree/js/app.js
+++ works/23Degree/js/app.js
@@ -159,6 +159,59 @@ window.addEventListener('DOMContentLoaded', () => {
 
     };
     sliders('.quote-item-slider', '', '', '');
+
+    //Smooth Scroll
+    const scrolling = (upSelector) => {
+        //Функция PageUp
+        const upElem = document.querySelector(upSelector);
+
+        window.addEventListener('scroll', () => {
+            if (document.documentElement.scrollTop > 1650) {
+                upElem.classList.add('animated', 'fadeIn');
+                upElem.classList.remove('fadeOut');
+            } else {
+                upElem.classList.add('fadeOut');
+                upElem.classList.remove('fadeIn');
+            }
+        });
+
+        //Scrolling with request animation frame
+        let links = document.querySelectorAll('[href^="#"]'),
+            speed = 0.2;
+
+        links.forEach(link => {
+            link.addEventListener('click', function (event) {
+                event.preventDefault();
+
+                let widthTop = document.documentElement.scrollTop,
+                    hash = this.hash,
+                    toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                    start = null;
+
+                requestAnimationFrame(step);
+
+                function step(time) {
+                    if (start === null) {
+                        start = time;
+                    }
+
+                    let progress = time - start,
+                        r = (toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock));
+
+                    document.documentElement.scrollTo(0, r);
+
+                    if (r !== widthTop + toBlock) {
+                        requestAnimationFrame(step)
+                    } else {
+                        location.hash = hash;
+                    }
+                }
+            });
+        });
+    };
+    scrolling('.pageup');
+
+
 });
 
 
--- works/irvas/js/app.js
+++ works/irvas/js/app.js
@@ -75,7 +75,6 @@ let tabs = () => {
 
 tabs();
 
-
 //Timer
 let timer = () => {
 
@@ -141,6 +140,66 @@ let timer = () => {
 };
 
 timer();
+
+//Forms
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input');
+
+    const message = {
+        loading: "Загрузка...",
+        success: "Спасибо! Скоро мы с вами свяжемся",
+        failure: "Что-то пошло не так..."
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            // statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormDate(item);
+
+            postData('server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    statusMessage.textContent = message.failure;
+                })
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+
+        });
+    });
+
+
+};
+
+forms();
 
 
 
