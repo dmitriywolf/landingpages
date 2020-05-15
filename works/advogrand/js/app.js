91b8c8fd56055d69a9ce379e9ce19e7cb8681ed4
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-05-15 21:27:34 +0300

    update advogrand

--- works/advogrand/js/app.js
+++ works/advogrand/js/app.js
@@ -28,7 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     /*Modals*/
     const modals = () => {
-        function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+
+        let btnPress = false;
+
+        function bindModal(triggerSelector, modalSelector, closeSelector) {
 
             const trigger = document.querySelectorAll(triggerSelector),
                 modal = document.querySelector(modalSelector),
@@ -42,6 +45,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         e.preventDefault();
                     }
 
+                    btnPress = true;
+
                     //Скрываем все открытые окна если такие есть
                     windows.forEach(item => {
                         item.classList.remove('show');
@@ -54,11 +59,6 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             close.addEventListener('click', () => {
-                //Скрываем все открытые окна если такие есть
-                windows.forEach(item => {
-                    item.classList.remove('show');
-                });
-
                 modal.classList.remove('show');
                 document.body.style.overflow = '';
                 document.body.style.marginRight = '0px';
@@ -66,31 +66,37 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             modal.addEventListener('click', (e) => {
-                if (e.target === modal && closeClickOverlay) {
+                if (e.target === modal) {
                     windows.forEach(item => {
                         item.classList.remove('show');
                     });
 
+                    modal.classList.remove('show');
                     document.body.style.overflow = '';
                     document.body.style.marginRight = '0px';
                 }
             });
         }
 
-        //
-        // function showModalBuTime(selector, time) {
-        //     setTimeout(function () {
-        //         document.querySelector(selector).classList.add('show', 'animated', 'bounceIn');
-        //         document.body.classList.add('show-popup');
-        //         document.querySelector(selector).addEventListener('click', (e) => {
-        //             let elem = e.target;
-        //             if (elem === document.querySelector(selector) || elem.closest('.popup__close')) {
-        //                 document.querySelector(selector).classList.remove('show');
-        //                 document.body.classList.remove('show-popup');
-        //             }
-        //         })
-        //     }, time);
-        // }
+        function showModalByTime(selector, time) {
+            setTimeout(function () {
+
+                let display;
+
+                document.querySelectorAll('.popup').forEach(item => {
+                    if (getComputedStyle(item).display !== 'none') {
+                        display = 'block';
+                    }
+                });
+
+                if (!display) {
+                    document.querySelector(selector).classList.add('show');
+                    document.body.style.overflow = 'hidden';
+                    let scroll = calcScroll();
+                    document.body.style.marginRight = `${scroll}px`;
+                }
+            }, time);
+        }
 
         //Получаем ширину скролла
         function calcScroll() {
@@ -108,18 +114,213 @@ document.addEventListener('DOMContentLoaded', () => {
             return scrollWidth;
         }
 
+        //Показывать окно когда пользователь пролистает до конца не нажмет ни одну кнопку
+        function openByScroll(selector) {
+            window.addEventListener('scroll', () => {
+                let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+
+                if (!btnPress && (window.pageYOffset + document.documentElement.clientHeight >= scrollHeight)) {
+                    document.querySelector(selector).click();
+                }
+            });
+        }
 
         bindModal('.button--callback', '.popup--callback', '.popup--callback .popup__close');
         bindModal('.button--consultation', '.popup--consultation', '.popup--consultation .popup__close');
+        bindModal('.button--feature', '.popup--feature', '.popup--feature .popup__close');
 
-        // showModalBuTime('.popup--feature', 10000);
+        showModalByTime('.popup--callback', 60000);
+
+        openByScroll('.button--feature');
     };
 
     modals();
 
+    /*Smooth Scroll*/
+    const scrolling = () => {
+        /*PageUp*/
+        let upElement = document.querySelector('.pageup');
+        window.addEventListener('scroll', () => {
 
-    /*Filter
-    * =====================================*/
+            if (document.documentElement.scrollTop > 1500) {
+                upElement.classList.add('fadeIn');
+                upElement.classList.remove('fadeOut');
+
+            } else {
+                upElement.classList.add('fadeOut');
+                upElement.classList.remove('fadeIn');
+            }
+        });
+
+        //Вспомогательные переменные для кроссбраузерности
+        const element = document.documentElement,
+            body = document.body;
+
+        //Якоря
+        let anchors = document.querySelectorAll('[href^="#"]');
+
+        //Подсчет расстояния скролинга
+        const calcScroll = () => {
+
+            anchors.forEach(item => {
+                item.addEventListener('click', function (event) {
+
+                    console.log(item.hash);
+
+                    let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+                    if (this.hash !== '') {
+                        event.preventDefault();
+
+                        //Элемент к которому будет произведен скрол
+                        let hashElement = document.querySelector(this.hash);
+                        let hashElementTop = 0;
+
+                        while (hashElement.offsetParent) {
+                            hashElementTop += hashElement.offsetTop;
+                            hashElement = hashElement.offsetParent;
+                        }
+
+                        hashElementTop = Math.round(hashElementTop);
+
+                        smoothScroll(scrollTop, hashElementTop, this.hash);
+                    }
+                });
+            })
+
+        };
+
+        //Функция скролинга
+        const smoothScroll = (from, to, hash) => {
+
+            let timeInterval = 1,
+                prevScrollTop,
+                speed;
+
+            //Скорость
+            if (to > from) {
+                speed = 30;
+            } else {
+                speed = -30;
+            }
+
+            //Анимация скролла
+            let move = setInterval(function () {
+                let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+                if (
+                    prevScrollTop === scrollTop ||
+                    (to > from && scrollTop >= to) ||
+                    (to < from && scrollTop <= to)
+                ) {
+                    clearInterval(move);
+                    history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+                } else {
+                    body.scrollTop += speed;
+                    element.scrollTop += speed;
+                    prevScrollTop = scrollTop;
+                }
+
+            }, timeInterval);
+        };
+
+        calcScroll();
+    };
+
+    scrolling();
+
+
+    /*Tariffs*/
+    const tariffs = () => {
+
+        let tariffsContent = document.querySelector('.tariffs__list');
+        let tariffLinks = document.querySelectorAll('.tariffs-menu__link');
+
+        tariffLinks.forEach(item => {
+            item.addEventListener('click', (e) => {
+                tariffLinks.forEach(item => {
+                    item.classList.remove('is-active');
+                });
+                item.classList.add('is-active');
+                tariffsContent.classList.toggle('horizontally');
+            });
+        });
+    };
+
+    tariffs();
+
+    /*Forms*/
+    const forms = () => {
+
+        //Все формы и поля
+        const form = document.querySelectorAll('form'),
+            inputs = document.querySelectorAll('input'),
+            textareas = document.querySelectorAll('textarea');
+
+
+        //Объект сообщений
+        const message = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Мы свяжемся с вами в течениии 5 минут!',
+            failure: 'Что то пошло не так...'
+        };
+
+        //Функция отправки запроса
+        const postData = async (url, data) => {
+            document.querySelector('.status').textContent = message.loading;
+
+            let res = await fetch(url, {
+                method: "POST",
+                body: data
+            });
+
+            return await res.text();
+        };
+
+        //Функция очистки полей
+        const clearFields = () => {
+            inputs.forEach(item => {
+                item.value = "";
+            });
+
+            textareas.forEach(item => {
+                item.value = "";
+            });
+        };
+
+        form.forEach(item => {
+            item.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                //Блок показа ответа
+                let statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                item.appendChild(statusMessage);
+
+                //Собираем все данные с формы
+                let formData = new FormData(item);
+
+                //Отправляем запрос на сервер
+                postData('server.php', formData)
+                    .then(res => {
+                        console.log(res);
+                        statusMessage.textContent = message.success;
+                    })
+                    .catch(() => statusMessage.textContent = message.failure)
+                    .finally(() => {
+                        clearFields();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 5000);
+                    });
+
+            });
+        });
+
+
+    };
+
+    forms();
 
 
 });
